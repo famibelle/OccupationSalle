37f3e216d3b96cfcc7458bf5610a3379b64c93a7
Famibelle Médhi <famibelle@users.noreply.github.com>
2015-09-18 16:46:45 +0200

    Update disponible.php

--- disponible.php
+++ disponible.php
@@ -1,16 +1,25 @@
 <?php
+// Specify your sqlite database name and path
 $datapie = array();
 $dbname = "sqlite:/var/www/PIRlog.db";
 $db = new PDO($dbname);
 
 $query = "SELECT strftime('%s','now','localtime') maintenant, strftime('%s',timestamp) enregistrement FROM TxOccupationBox WHERE motion = 1 ORDER BY timestamp DESC LIMIT 1;";
-
 $result = $db->query($query);
 
 $result->setFetchMode(PDO::FETCH_ASSOC);
 while ($row = $result->fetch()) {
         extract($row);
         $secondes = $maintenant - $enregistrement;
+        $Heures = floor($secondes / 3600);
+        $Minutes = ($secondes / 60) % 60;
+        $Secondes = $secondes % 60;
         echo "Aucun mouvement depuis " . gmdate("H:i:s", $secondes);
+        if ($secondes> 300) {
+                echo "<p>le box est vide depuis " . $Heures . " heures " . $Minutes .  " minutes et "  . $Secondes . " secondes";
+        }
+        else {
+                echo "<p>le box est occupé";
+        }
 }
 ?>
