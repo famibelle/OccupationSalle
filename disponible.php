e8c272c5534f37da9ccea4b1a9c1d15a5dae0f18
Famibelle Médhi <famibelle@users.noreply.github.com>
2015-09-22 23:04:32 +0200

    corrections de bugs

--- disponible.php
+++ disponible.php
@@ -4,24 +4,24 @@ $datapie = array();
 $dbname = "sqlite:/var/www/PIRlog.db";
 $db = new PDO($dbname);
 
-$query = "SELECT strftime('%s','now','localtime') maintenant, strftime('%s',$
+$query = "SELECT strftime('%s','now','localtime') maintenant, strftime('%s',timestamp) enregistrement FROM TxOccupationBox WHERE motion = 1 ORDER BY timestamp DESC LIMIT 1;";
 $result = $db->query($query);
 
 $result->setFetchMode(PDO::FETCH_ASSOC);
 while ($row = $result->fetch()) {
-        extract($row);
-        $secondes = $maintenant - $enregistrement;
-        $Heures = floor($secondes / 3600);
-        $Minutes = ($secondes / 60) % 60;
-        $Secondes = $secondes % 60;
-        $datapie[] = array("Heures" => $Heures, "Minutes" => $Minutes , "Sec$
-/*      echo "Aucun mouvement depuis " . gmdate("H:i:s", $secondes);
-        if ($secondes> 300) {
-                echo "<p>le box est vide depuis " . $Heures . " heures " . $$
-        }
-        else {
-                echo "<p>le box est occupé";
-        }*/
+	extract($row);
+	$secondes = $maintenant - $enregistrement;
+	$Heures = floor($secondes / 3600);
+	$Minutes = ($secondes / 60) % 60;
+	$Secondes = $secondes % 60;
+	$datapie[] = array("Heures" => $Heures, "Minutes" => $Minutes , "Secondes" => $Secondes);
+/*	echo "Aucun mouvement depuis " . gmdate("H:i:s", $secondes);
+	if ($secondes> 300) {
+		echo "<p>le box est vide depuis " . $Heures . " heures " . $Minutes .  " minutes et "  . $Secondes . " secondes";
+	}
+	else {
+		echo "<p>le box est occupé";
+	}*/
 }
 
 $data = json_encode($datapie);
