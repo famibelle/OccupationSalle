fae92621b60e2d4afec680c3341eb79742e55488
famibelle <medhi.famibelle@orange.com>
2015-09-30 23:03:43 +0200

    le script exporte les données au format csv

--- export.php
+++ export.php
@@ -1,17 +1,41 @@
 <?php
-// Specify your sqlite database name and path
-$datapie = array();
+// thanks to http://stackoverflow.com/questions/3986469/export-sqlite-table-using-php
+$Query = "SELECT * FROM TxOccupationBox WHERE motion = 1;";
 $dbname = "sqlite:/var/www/PIRlog.db";
-$db = new PDO($dbname);
 
-$query = "SELECT * FROM TxOccupationBox WHERE motion = 1;";
-$result = $db->query($query);
+// Set headers to make the browser download the results as a csv file
+header("Content-type: text/csv");
+header("Content-Disposition: attachment; filename=DonnéesBrutes.csv");
+header("Pragma: no-cache");
+header("Expires: 0");
 
-$result->setFetchMode(PDO::FETCH_ASSOC);
-while ($row = $result->fetch()) {
-	extract($row);
-	$datapie[] = array("Timestamp" => $timestamp, "Motion" => $motion);
+// Connect to DB
+$conn = new PDO($dbname);
+
+// Query
+$query = $conn->query($Query);
+
+// Fetch the first row
+$row = $query->fetch(PDO::FETCH_ASSOC);
+
+// If no results are found, echo a message and stop
+if ($row == false){
+    echo "No results";
+    exit;
 }
-$data = json_encode($datapie);
-echo $data;
+
+// Print the titles using the first line
+print_titles($row);
+// Iterate over the results and print each one in a line
+while ($row != false) {
+    // Print the line
+  echo implode(array_values($row), ",") . "\n";
+    // Fetch the next line
+  $row = $query->fetch(PDO::FETCH_ASSOC);
+}
+
+// Prints the column names
+function print_titles($row){
+    echo implode(array_keys($row), ",") . "\n";
+}
 ?>
