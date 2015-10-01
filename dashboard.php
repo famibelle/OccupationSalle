d8978a57c25312b07af9b19e342cd7dde14a07d4
Famibelle Médhi <famibelle@users.noreply.github.com>
2015-10-01 18:00:11 +0200

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<?php
+$dbname = "sqlite:/var/www/PIRlog.db";
+$row = array();
+for ($jour = 0; $jour < 7; $jour++) {
+		for ($heure = 0; $heure < 24; $heure++) {
+		// Connect to DB
+		$conn = new PDO($dbname);
+
+		// Query
+		$Query = "SELECT strftime('%w', timestamp) jour, strftime('%H', timestamp) heure, sum(motion) as mouvements FROM TxOccupationBox WHERE jour ='" . $jour$
+		echo $Query . "\n";
+		$query = $conn->query($Query);
+		$row[$jour][$heure] = $query->fetch(PDO::FETCH_ASSOC);
+	}
+}
+echo json_encode($row);
+?>
